4527a70e3187ab1ba6ce7477fc5d6e7dff4cd481
marissab13 <marissab13@gmail.com>
2017-12-02 15:35:40 -0800

    starting to add event listener for dice object on page

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -1,4 +1,5 @@
 /*
+Pork It over - The fun, gambling Piggy Game
 GAME RULES:
 
 - The game has 2 players, playing in rounds
@@ -6,31 +7,35 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
-
-Pork It over - The fun, gambling Piggy Game
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, 
 
- scores = [0,0];
- roundScore = 0;
- activePlayer = 0;
-
- dice = Math.floor(Math.random() * 6) + 1;
- console.log(dice);
-
- document.querySelector('#current-' + activePlayer).innerHTML = dice;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
 
 
-
-
- var x = document.querySelector('#score-0').textContent;
- console.log(x);
 
  document.querySelector('.dice').style.display = 'none';
 
- document.querySelector('.btn-roll').addEventListener('click')
+ document.getElementById('score-0').textContent = '0';
+ document.getElementById('score-1').textContent = '0';
+ document.getElementById('current-0').textContent = '0';
+ document.getElementById('current-1').textContent = '0';
+
+ document.querySelector('.btn-roll').addEventListener('click', function() {
+ 	// need a random number
+	  var dice = Math.floor(Math.random() * 6) + 1;
+	 
+	 	//display the result
+	 	var diceDOM = 	document.querySelector('.dice');
+	 	diceDOM.style.display = 'block';
+	 	diceDOM.src = 'dice-' + dice + '.png';
+	 
+	 	//update the round score only if the rolled number is not 1
+
+ });
 
 
 
@@ -40,5 +45,5 @@ var scores, roundScore, activePlayer, dice;
 
 
 
-
-
+// var x = document.querySelector('#score-0').textContent;
+//document.querySelector('#current-' + activePlayer).innerHTML = dice;
